796c8bfa6ccd7b1e5a219595ea03dc73ebaac0cf
JahangirEdorpon <jahangir@edorpon.com>
2020-12-26 11:22:25 +0600

    Design

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
         autoPlay: 3000, //Set AutoPlay to 3 seconds
         animateOut: 'slideOutDown',
         animateIn: 'flipInX',
-   
+        loop:true,
+        nav:true,
+        responsive: { 0: { items: 1 }, 480: { items: 1 }, 992: { items: 1 } },
         items : 1,
-        itemsDesktop : [1199,1],
-        itemsDesktopSmall : [979,1],
-        itemsTeblet: [500,1]
+        
    
     });
 
@@ -21,5 +21,46 @@ $(document).ready(function() {
     //     stagePadding:30,
     //     smartSpeed:450
     // });
+
+
+    $('.fancybox').fancybox({
+        padding   : 0,
+        maxWidth  : '100%',
+        maxHeight : '100%',
+        width   : 560,
+        height    : 315,
+        autoSize  : true,
+        closeClick  : true,
+        openEffect  : 'elastic',
+        closeEffect : 'elastic'
+      });
+
+   
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
    
   });
+
+
+  
